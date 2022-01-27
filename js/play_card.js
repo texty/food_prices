38469b3e75394000847d6a37b43fb34a4f92819d
YevhDr <evgesha.drozdova@gmail.com>
2022-01-27 16:07:38 +0200

    play cards

--- js/play_card.js
+++ js/play_card.js
@@ -76,15 +76,15 @@ d3.csv("data/play_cards.csv").then(function(cards){
                 };       
 
         //відправляємо записаний рядок
-        d3.json('url', {
+     /*    d3.json('url', {
             method:"POST",
             body: JSON.stringify(row),
             headers: {
                 "Content-Type": "application/json; charset=UTF-8"
             }
         }).then(function(row){    
-            //submitAnswer(); 
-        }) 
+            submitAnswer(); 
+        })  */
 
         submitAnswer(); 
         
